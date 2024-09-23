6bfed958ea2a8bb68b15d0476b5aff05af97be24
David-Portugal-Ferreira <davidportugal44@gmail.com>
2024-09-23 22:05:45 +0100

    Get more weather images

---
+++ src/modules/images_manipulation.js
@@ -0,0 +1,26 @@
+const moonPhases = {
+  crescente_1: `src/images/moon-phases/crescente_1_freepik.png`,
+  crescente_2: `src/images/moon-phases/crescente_2_freepik.png`,
+  crescente_3: `src/images/moon-phases/crescente_3_freepik.png`,
+  full_moon: `src/images/moon-phases/lua_cheia_freepik.png`,
+  minguante_1: `src/images/moon-phases/minguante_1_freepik.png`,
+  minguante_2: `src/images/moon-phases/minguante_2_freepik.png`,
+  minguante_3: `src/images/moon-phases/minguante_3_freepik.png`,
+  new_moon: `src/images/moon-phases/lua_nova_freepik.png`,
+};
+
+const weather = {
+  cloud: `src/images/weather/cloud_iconixar.png`,
+  cloudy: `src/images/weather/cloudy_iconixar.png`,
+  cloudy_rain: `src/images/weather/cloudy_rain_iconixar.png`,
+  fog: `src/images/weather/fog_iconixar.png`,
+  humidity: `src/images/weather/humidity_iconixar.png`,
+  snowy: `src/images/weather/snowy_iconixar.png`,
+  storm: `src/images/weather/storm_iconixar.png`,
+  sun: `src/images/weather/sun_iconixar.png`,
+  warm_1: `src/images/weather/warm_1_iconixar.png`,
+  warm_2: `src/images/weather/warm_2_iconixar.png`,
+  warm_3: `src/images/weather/warm_3_iconixar.png`,
+};
+
+export { moonPhases, weather }
